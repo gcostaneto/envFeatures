ddf49f32fa24c1db80e988adf9710acc08cd9573
Germano Costa Neto <germano.cneto@gmail.com>
2023-09-22 15:02:38 -0400

    Update fitMLRGEA_fun.R

--- src/fitMLRGEA_fun.R
+++ src/fitMLRGEA_fun.R
@@ -4,13 +4,16 @@
 #' Title..........Multionomial Logistic Regression (MLR) for Genomic-Environment Association (GEA) #'
 #' Function....... fitMLRGEA(), version 0.0.1 (Sep 15 2023)                                        #'
 #' Created at..... 06-03-2023                                                                      #'
-#' Updated at..... 09-15-2023                                                                      #'
+#' Updated at..... 09-22-2023                                                                      #'
 #' Author: G.Costa-Neto, germano.cneto<at>gmail.com                                                #'
-#' ....... Contributors: P. Bradbury (model testing, hypothesis)                                   #'
+#' ....... Contributors: P. Bradbury (model testing, hypothesis), S.K-Hsu (review, usage)          #'
 #'                                                                                                 #'
 #'##################################################################################################'
-#'
-# packages
+#' older versions:
+#' v 0.0.0 ... 06-33-2023 
+#' v 0.0.1 ... 09-13-2023   
+#' v 0.0.2 ... 09-15-2023   
+#'##################################################################################################'
 
 fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
                       output.path = NULL,  # output directory. If null, getwd()
@@ -30,7 +33,7 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
     message(paste0('========================================================================'))
     message(paste0('Multinomial Logistic Regresion for Genomic-Environment Associations (MLR-GEA)'))
     #  message(paste0('Author: G. Costa-Neto'))
-    message(paste0('fitMLRGEA() function, v0.0.1 (Sep 15 2023)'))
+    message(paste0('fitMLRGEA() function, v0.0.3 (Sep 22 2023)'))
     message(paste0('-========================================================================'))
     
   }
@@ -48,18 +51,26 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
   
   if (!requireNamespace("ordinal", quietly = TRUE)) {
     utils::install.packages("ordinal")
+    #   require("ordinal")
   }
   
   if (!requireNamespace("doParallel", quietly = TRUE)) {
     utils::install.packages("doParallel")
   }
   
+  if (!requireNamespace("parallel", quietly = TRUE)) {
+    utils::install.packages("parallel")
+    require("parallel")
+  }
+  
   if (!requireNamespace("foreach", quietly = TRUE)) {
     utils::install.packages("foreach")
+    # require("foreach")
   }
   
   if (!requireNamespace("reshape2", quietly = TRUE)) {
     utils::install.packages("reshape2")
+    # require("reshape2")
   }
   
   
@@ -67,17 +78,69 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
   # Building the Models     #####
   #'-------------------------------------------------------------------------------------------------
   
+
+  message(paste0('Start checking data at ......', Sys.time(),'\n'))
+  
+  if(is.null( envo_X))
+  { 
+    stop('You need to add some environmental features as envo_X')
+  }
+  
+  if(!is.null( envo_X))
+  {
+    if(!is.data.frame(envo_X))
+    {
+      if(is.numeric(envo_X))
+      {
+        message('Warning: unique  envo_X. Renamed as "envCOV" ')
+        
+        envo_X <- data.frame(envCOV = envo_X)
+        if(isFALSE(is.null(rownames(geno_Y))))
+        {
+          message('Warning: rownames(envo_X) <- rownames(geno_Y)')
+          rownames(envo_X) <- rownames(geno_Y)
+        }
+        if(isTRUE(is.null(rownames(geno_Y))))
+        {
+          stop('You need to specify the rownames (taxa) for geno_Y')
+        }
+      }
+    }
+  }
+  
+
+  
+
   
   if(!is.null( geno_X))
   {
-    common_rows <- 
-      base::intersect(rownames(geno_X),rownames( envo_X)) %>% 
-      base::intersect(rownames(  geno_Y)) %>%
-      sort()
+    if(!is.data.frame(geno_X))
+    {
+      if(is.numeric(geno_X))
+      {
+        message('Warning: uniquegeno_X. Renamed as "genoCOV" ')
+        
+        geno_X <- data.frame(genoCOV = geno_X)
+        if(isFALSE(is.null(rownames(geno_Y))))
+        {
+          message('Warning: rownames(envo_X) <- rownames(geno_Y)')
+          rownames(envo_X) <- rownames(geno_Y)
+        }
+        if(isTRUE(is.null(rownames(geno_Y))))
+        {
+          stop('You need to specify the rownames (taxa) for geno_Y')
+        }
+      }
+    }
     
-    geno_Y <- geno_Y[common_rows,]
-    envo_X  <- envo_X[common_rows,]
-    geno_X   <- geno_X[common_rows,]
+#   common_rows <- 
+#      base::intersect(rownames(geno_X),rownames( envo_X)) %>% 
+#      base::intersect(rownames(  geno_Y)) %>%
+#      sort()
+    
+#    geno_Y <- geno_Y[common_rows,]
+#    envo_X  <- envo_X[common_rows,]
+#    geno_X   <- geno_X[common_rows,]
     
     EnvTraits = colnames(  envo_X )
     GCovariates = colnames(  geno_X )
@@ -96,14 +159,16 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
   
   if(is.null(geno_X))
   {
-    common_rows <- 
-      base::intersect(rownames(geno_Y),rownames( envo_X))
+    message("Not running H3 -- no geno_X available")
     
-    geno_Y  <- geno_Y[common_rows,]
-    envo_X  <- envo_X[common_rows,]
+#   common_rows <- 
+#      base::intersect(rownames(geno_Y),rownames( envo_X))
+    
+#   geno_Y  <- geno_Y[common_rows,]
+#    envo_X  <- envo_X[common_rows,]
     geno_X  <- data.frame(dummt=matrix(NA,nrow = nrow(geno_Y)))
     
-    EnvTraits = colnames(  envo_X )
+    EnvTraits = names(  envo_X )
     
     
     model_inputs = 
@@ -126,16 +191,17 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
   {
     if(isTRUE(parallel))
     {
-      cl <- makeCluster(detectCores() - 1)
-      registerDoParallel(cl)
+      cl <- parallel::makeCluster(detectCores() - 1)
+      doParallel::registerDoParallel(cl)
     }
   }
   if(!is.null(n.core))
   {
-    cl <-  makeCluster(n.core)
-    registerDoParallel(cl)
+    cl <-  parallel::makeCluster(n.core)
+    doParallel::registerDoParallel(cl)
   }
   
+  message(paste0('End checking data at ......', Sys.time(),'\n'))
   
   #'-------------------------------------------------------------------------------------------------
   # Running the models     #####
@@ -143,7 +209,7 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
   
   
   start = Sys.time()
-  message(paste0('started at ......',start,'\n'))
+  message(paste0('Model started at ......',start,'\n'))
   
   
   output_MLRGEA =
@@ -193,14 +259,14 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
                                        n.parameters_full = length(model_inputs),
                                        
                                        # Model AIC
-                                       AIC_null     =   H1$AIC[1],
-                                       AIC_geno     =   H2$AIC[1],
-                                       AIC_full     =   H1$AIC[2],
+                                       AIC_null     =   NA,
+                                       AIC_geno     =   NA,
+                                       AIC_full     =   NA,
                                        
                                        # Model p-value
-                                       log10p_H1 = -log10( H1$`Pr(>Chisq)`[2]),
-                                       log10p_H2 = -log10( H2$`Pr(>Chisq)`[2]),
-                                       log10p_H3 = -log10( H3$`Pr(>Chisq)`[2]),
+                                       log10p_H1 = NA,
+                                       log10p_H2 = NA,
+                                       log10p_H3 = NA,
                                        
                                        # McFadden's pseudo R-sq
                                        McF.pR2_H1   = NA,
@@ -208,7 +274,15 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
                                        McF.pR2_H3   = NA)
               
               
+              # Model AIC
+              output.stat$AIC_null  =   H1$AIC[1]
+              output.stat$AIC_geno  =   H2$AIC[1]
+              output.stat$AIC_full  =   H1$AIC[2]
               
+              # Model p-value
+              output.stat$  log10p_H1 = -log10( H1$`Pr(>Chisq)`[2])
+              output.stat$  log10p_H2 = -log10( H2$`Pr(>Chisq)`[2])
+              output.stat$  log10p_H3 = -log10( H3$`Pr(>Chisq)`[2])
               # McFadden's pseudo R-sq
               output.stat$ McF.pR2_H1   = try(100*round(1 - full_model$logLik/null_model$logLik,4))
               output.stat$ McF.pR2_H2   = try(100*round(1 - full_model$logLik/geno_model$logLik,4))
@@ -223,8 +297,8 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
               # gcostaneto note: here I only pull the partial p and effects for the full model.
               # you can modify this code to pull for the env model, for instance. There is no limit for the imagination ;)
               
-              output.partial       = summary(full_model)$coefficients
-              output.partial       = data.frame( output.partial[which(rownames( output.partial) %in% model_inputs ),])
+              output.partial       = as.data.frame(summary(full_model)$coefficients)
+              output.partial       = output.partial[which(rownames( output.partial) %in% model_inputs ),]
               
               output.partial <-  data.frame(Feature = rownames( output.partial), output.partial[,c(1:2,4)] )
               names( output.partial)[2:4] = c('effect','se_effect','p_value')
@@ -232,8 +306,15 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
               
               
               # maximum log10p
-              nulllog =  output.partial$log10[which( output.partial$log10 == Inf)] 
-              if(!is.null( nulllog ))  output.partial$log10[which( output.partial$log10 == Inf)] = 323
+              nulllog =  any(output.partial$log10[which( output.partial$log10 == Inf)])
+              if(isTRUE( nulllog ))  
+              {
+                max_value = max(output.partial$log10[-which( output.partial$log10 == Inf)],na.rm=T)
+                output.partial$log10[which( output.partial$log10 == Inf)] =   max_value 
+              }
+              
+              
+              
               
               output.partial <- 
                 output.partial %>% 
@@ -246,8 +327,12 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
               #' Model Step 4: Exporting ;)
               #'---------------------------------------------------------
               
-              data.table::fwrite(       output.partial, paste0(output.path,'/model_partial',output_name ,".csv"),append = T)
-              data.table::fwrite(       output.stat, paste0(output.path,'/model_statistics',output_name ,".csv"),append = T)
+              if(isTRUE(save))
+              {
+                data.table::fwrite(       output.partial, paste0(output.path,'/model_partial',output_name ,".csv"),append = T)
+                data.table::fwrite(       output.stat, paste0(output.path,'/model_statistics',output_name ,".csv"),append = T)
+              }
+              
               
               output <-
                 output.stat %>% 
@@ -264,10 +349,15 @@ fitMLRGEA <- function(home.path = NULL,  # home directory. If null, getwd()
   stopCluster(cl) 
   
   end = Sys.time()
-  message(paste0('ended at ......',end,'\n'))
+  message(paste0('Model ended at ......',end,'\n'))
   
-  message(paste0('saving files at......',output.path,'\n'))
-  data.table::fwrite(  output_MLRGEA, paste0(output.path,'/Full_Results_',output_name ,".csv"))
+  if(isTRUE(save))
+  {
+    message(paste0('saving files at......',output.path,'\n'))
+    data.table::fwrite(  output_MLRGEA, paste0(output.path,'/Full_Results_',output_name ,".csv"))
+  }
+  
   return(  output_MLRGEA)
 }
+
 
